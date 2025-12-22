f35dfd01b5b179fef442a89aaddaa472871ec148
Griger5 <griger04@gmail.com>
2025-12-22 20:14:25 +0100

    add: hello world with nuxt structure

---
+++ app/components/nav-bar.vue
@@ -0,0 +1,12 @@
+<template>
+	<nav class="container">
+		<ul>
+			<li><strong>shrimp</strong></li>
+			<li><a href="#">About</a></li>
+		</ul>
+		<ul>
+			<li><a href="#">New Image</a></li>
+			<li><a href="#">Your account</a></li>
+		</ul>
+	</nav>
+</template>
